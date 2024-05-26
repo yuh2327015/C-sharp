14adac3f8843b360ff82de82d28be59dfbc3733d
yuh2327015 <www9216@naver.com>
2024-05-26 14:30:36 +0900

    c#과제

---
+++ 과제/우성윤(202327015)_Project#1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace 우성윤_202327015__Project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
