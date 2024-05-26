999bad0a1b4af96090b1ab8cb6e4ac6bc26057ab
yuh2327015 <www9216@naver.com>
2024-05-26 14:49:39 +0900

    과제 업데이트

---
+++ 과제/우성윤(202327015)_Project#1/ApplianceItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using 우성윤_202327015__Project_1;
+
+namespace 우성윤_202327015__Project_1
+{
+    public enum ApplianceType
+    {
+        Washer,
+        Dryer,
+        Dishwasher,
+        Oven,
+        Refrigerator
+    }
+
+    public class ApplianceItem : Item
+    {
+        public ApplianceType Type { get; set; }
+
+        public ApplianceItem(string description, decimal price, string sku, ApplianceType type)
+            : base(description, price, sku)
+        {
+            Type = type;
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Appliance Item: {Description}, Price: {Price:C}, Sku: {Sku}, Type: {Type}");
+        }
+    }
+}
--- 과제/우성윤(202327015)_Project#1/Program.cs
+++ 과제/우성윤(202327015)_Project#1/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace 우성윤_202327015__Project_1
+﻿using System;
+using 우성윤_202327015__Project_1;
+
+namespace 우성윤_202327015__Project_1
 {
-    internal class Program
+    class Program
     {
+        //프로젝트 #1
+        //제출자 : 우성윤(202327015)
+        //제출일 : 2024.05.26
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ItemList itemList = new ItemList();
+
+            // CSV 파일 경로
+            string filePath = "items.csv";
+
+            // CSV 파일에서 아이템 로드
+            itemList.LoadItemsFromCsv(filePath);
+
+            // 항목 정보 출력
+            itemList.DisplayItems();
         }
     }
 }
