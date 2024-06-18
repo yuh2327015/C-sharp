711c723f50caf243785985a5eac5f56015cff9b4
yuh2327015 <www9216@naver.com>
2024-06-18 14:26:16 +0900

    FEX

---
+++ 6월17일/4/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Diagnostics;
+using System.Diagnostics;
+
+namespace _4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnJoin_Click(object sender, RoutedEventArgs e)
+        {
+            string str = "";
+            foreach (CheckBox child in spAgree.Children)
+            {
+                if (child.IsChecked == true)
+                    str += child.Content.ToString() + "\n";
+            }
+            foreach (RadioButton child in spAge.Children)
+            {
+                if (child.IsChecked != null)
+                    if (child.IsChecked.Value == true)
+                        str += "나이: " + child.Content.ToString() + "\n";
+            }
+            foreach (RadioButton child in spJob.Children)
+            {
+                if (child.IsChecked !=null)
+                    if(child.IsChecked.Value == true)
+                    {
+                        if (child.GroupName == "grpSex")
+                            str += "성별: " + child.Content.ToString() + "\n";
+                        else
+                            str += "직업: " + child.Content.ToString() + "\n";
+                    }
+            }
+            MessageBox.Show(str, "입력정보 확인");
+        }
+    }
+}
